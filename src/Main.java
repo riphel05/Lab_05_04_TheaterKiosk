126511de7d0771ff2c58db432817dfc95e2cd2c5
riphel05 <phelpsre@mail.uc.edu>
2022-09-25 22:37:49 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class
+Main {
+    public static void main(String[] args)
+    {
+        int userAge = 34;
+
+        if(userAge>=21)
+        {
+            System.out.println("You get a wristband");
+        }
+    }
+}
